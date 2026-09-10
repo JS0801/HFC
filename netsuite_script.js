fd832868e3947d6aa6817810180cc3b523138beb
JS0801 <jsuthar@oncloudconsulting.ca>
2026-09-10 16:39:16 -0400

    Update from NetSuite (Auto)

--- netsuite_script.js
+++ netsuite_script.js
@@ -2,7 +2,7 @@
   "id": "",
   "name": "Unknown",
   "recordType": "",
-  "exportedAt": "2026-09-10T20:39:07.849Z",
+  "exportedAt": "2026-09-10T20:39:14.420Z",
   "criteria": [],
   "columns": [],
   "sort": []

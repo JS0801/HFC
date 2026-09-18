16b32c88eb7215d3b431e504fa350e871fc68563
JS0801 <jsuthar@oncloudconsulting.ca>
2026-09-18 12:18:33 -0400

    Update from NetSuite (Auto)

--- buildXML.js
+++ buildXML.js
@@ -856,8 +856,7 @@ define(['N/ui/serverWidget', 'N/runtime', 'N/redirect', 'N/search', 'N/record', 
             var mm = (d.getMonth() + 1);
             var dd = (d.getDate());
             var yyyy = d.getFullYear();
-           // return mm + '/' + dd + '/' + yyyy;
-          return '09/10/2026';
+         return mm + '/' + dd + '/' + yyyy;
         }
 
         return {
